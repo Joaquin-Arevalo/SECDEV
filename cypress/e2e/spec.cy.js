f79b63a4a01b97adeb2bd5ad6426a917b68b82f4
Mark Abergos <114644434+Luminous-02@users.noreply.github.com>
2024-06-21 16:50:01 +0800

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -19,9 +19,35 @@ describe('Login Page', () => {
     cy.get('#error_issue').should('be.visible').and('contain', 'Incorrect Credentials!');
   });
 
+  // forgot password for no email found in database
+  it('Forgot password (email not found on database)', () => {
+  
+    // Click the login button
+    cy.get('#login-form > :nth-child(2) > button').click();
+
+    // enter valid username/email
+    cy.get('#for-pas-email').type('notfoundemail');
+    
+    // Assert that the error message is displayed
+    cy.get('#error_issue').should('contain', 'email not found! Register first');
+  });
+
+  // forgot password correct email
+  it('Forgot password (email found on database)', () => {
+  
+    // Click the login button
+    cy.get('#login-form > :nth-child(2) > button').click();
+
+    // enter valid username/email
+    cy.get('#for-pas-email').type('john.doe@example.com');
+    
+    // Assert that the error message is displayed
+    cy.get('#forgot-password-form').should('not.exist');
+  });
+
   it('Successful login', () => {
     // Enter valid username/email and password
-    cy.get('#email').type('alice.johnson@example.com');
+    cy.get('#email').type('john.doe@example.com');
     cy.get('#password').type('password123');
     
     // Click the login button
@@ -29,6 +55,21 @@ describe('Login Page', () => {
 
     // Assert that the login button is not visible
     cy.get('#login-button').should('not.exist');
+  })
+
+  it('Successful logout', () => {
+    // Enter valid username/email and password
+    cy.get('#email').type('john.doe@example.com');
+    cy.get('#password').type('password123');
+    
+    // Click the login button
+    cy.get('#login-button').click();
+
+    // Click the login button
+    cy.get('h2 > a').click();       ///click on the logout button (s)
+
+    // Assert that the login button is not visible
+    cy.get('#logout-button').should('not.exist');
   })
 
   
